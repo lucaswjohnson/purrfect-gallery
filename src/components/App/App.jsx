0870c470a3d4630a183e3fed05624c4188e78199
Lucas Johnson <lucaswj97@gmail.com>
2019-05-17 13:18:44 -0400

    start theme component and navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './App.scss'
 import Gallery from '../Gallery/Gallery'
+import Theme from '../Theme/Theme'
 
 const App = () => {
   return (
     <div className="home-container">
-      <div className="home-nav">Some text</div>
+      <div className="home-nav">
+        <div className="home-links">
+          <a onClick={() => console.log('hey')}>Linkie</a>
+          <a onClick={() => console.log('hey')}>Linkie</a>
+          <a onClick={() => console.log('hey')}>Linkie</a>
+          <a onClick={() => console.log('hey')}>Linkie</a>
+        </div>
+        <Theme />
+      </div>
       <Gallery />
     </div>
   )
---
+++ src/components/Theme/Theme.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Theme.scss'
+
+const Theme = () => {
+  return (
+    <div className="home-theme">
+      <div className="theme-header">
+        Theme
+      </div>
+      <div>
+        <button className="theme-button" />
+        <button className="theme-button" />
+        <button className="theme-button" />
+        <button className="theme-button" />
+      </div>
+    </div>
+  )
+}
+
+export default Theme
