65b702fcf8c7130662e4eb7bbf44b6d1f8155920
Lucas Johnson <lucaswj97@gmail.com>
2019-05-17 11:36:52 -0400

    more cleanup, add node-sass

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+import Gallery from '../Gallery/Gallery'
+
+const App = () => {
+  return (
+    <div className="home-container">
+      <div className="home-nav">Some text</div>
+      <Gallery />
+    </div>
+  )
+}
+
+export default App
