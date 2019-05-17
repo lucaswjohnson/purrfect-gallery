cd380cca2cad11fa0f081c38007bc3808795296e
Lucas Johnson <lucaswj97@gmail.com>
2019-05-17 10:03:48 -0400

    clean up react app defaults

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is about to be a super duper cool app
+      </header>
+    </div>
+  )
+}
+
+export default App
