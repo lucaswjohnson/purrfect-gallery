2320a771c157a4af10798f3c1bf183096fcb8d36
Lucas Johnson <lucaswj97@gmail.com>
2019-06-10 10:58:38 -0400

    map theme state to props

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.scss'
 import Gallery from '../Gallery/Gallery'
 import Theme from '../Theme/Theme'
 import { themes } from '../../themes'
-import { store } from '../../index'
 import { connect } from 'react-redux'
 
-class App extends Component {
-  state = {
-    theme: 'blue'
-  }
-
-  componentDidMount () {
-    store.subscribe(() => {
-      this.setState({
-        theme: store.getState().theme
-      })
-    })
-  }
-
-  getTheme = (type) => {
-    const { theme = 'blue' } = this.state
+const App = ({ theme = 'blue' }) => {
+  const getTheme = (type) => {
     const { primary, secondary, accent } = themes[theme]
     const isAccent = type === 'accent'
 
@@ -30,25 +16,29 @@ class App extends Component {
     }
   }
 
-  render () {
-    return (
-      <div className="home-container">
-        <div className="home-nav" style={this.getTheme('primary')}>
-          <div className="home-title" style={this.getTheme('accent')}>
-            Purrfect Gallery
-          </div>
-          <div className="home-links" style={this.getTheme('primary')}>
-            <a onClick={() => console.log('hey')}>Linkie</a>
-            <a onClick={() => console.log('hey')}>Linkie</a>
-            <a onClick={() => console.log('hey')}>Linkie</a>
-            <a onClick={() => console.log('hey')}>Linkie</a>
-          </div>
-          <Theme />
+  return (
+    <div className="home-container">
+      <div className="home-nav" style={getTheme('primary')}>
+        <div className="home-title" style={getTheme('accent')}>
+          Purrfect Gallery
         </div>
-        <Gallery />
+        <div className="home-links" style={getTheme('primary')}>
+          <a onClick={() => console.log('hey')}>Linkie</a>
+          <a onClick={() => console.log('hey')}>Linkie</a>
+          <a onClick={() => console.log('hey')}>Linkie</a>
+          <a onClick={() => console.log('hey')}>Linkie</a>
+        </div>
+        <Theme />
       </div>
-    )
+      <Gallery />
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    theme: state.themeReducer.theme
   }
 }
 
-export default connect()(App)
+export default connect(mapStateToProps)(App)
